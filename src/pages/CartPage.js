32619dfef66983220e9a57f9c8d3a6e57ce7c0ce
mohammad <96320586+Mohammad-Bereymi@users.noreply.github.com>
2022-11-02 21:33:04 +0330

    styling cart page

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,20 +1,41 @@
 import Layout from "../Layout/Layout";
 import { useCart } from "../Providers/CartProvider";
+import "./cartPage.css";
 
 const CartPage = () => {
   const { cart } = useCart();
+  if (!cart.length) {
+    return (
+      <Layout>
+        <main>
+          <h2>cart is empty !</h2>
+        </main>
+      </Layout>
+    );
+  }
   return (
     <Layout>
-      <main>
-        {cart.length ? (
-          cart.map((item) => (
-            <div key={item.id}>
-              <p>{item.name}</p>
-            </div>
-          ))
-        ) : (
-          <p>no item in cart</p>
-        )}
+      <main className="container">
+        <section className="cartCenter">
+          <section className="cartItemList">
+            {cart.map((item) => (
+              <div className="cartItem">
+                <div className="itemImg">
+                  <img src={item.image} alt={item.name} />
+                </div>
+                <div>{item.name}</div>
+                <div>{item.price * item.quantity}</div>
+                <div></div>
+                <div>
+                  <button>Remove</button>
+                  <button>{item.quantity}</button>
+                  <button>Add</button>
+                </div>
+              </div>
+            ))}
+          </section>
+          <section className="cartSummary">cart summary</section>
+        </section>
       </main>
     </Layout>
   );
